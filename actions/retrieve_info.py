eacb7af3dde4412a7e40397c8afa7cc8b8af620e
Bru0x11 <matteobrugnera737@gmail.com>
2023-03-03 19:04:23 +0100

    trying the API wrapper

--- actions/retrieve_info.py
+++ actions/retrieve_info.py
@@ -1,10 +1,14 @@
 """
-Use the API provided by IMBD to obtain all the necessary information from the dataset.
+Use the API provided to obtain all the necessary information from the dataset.
 Use this API to query the KB.
 """
 
-import imdb
+import requests
+import tmdbsimple as tmdb
 
-moviesDB = imdb.IMDB()
-
-print(dir(moviesDB))
+tmdb.API_KEY = "a3d485e7dbba8ea69c0d9041ab46207a"
+movie = tmdb.Movies(603)
+search = tmdb.Search()
+response = movie.info()
+query = search.movie(query='Tomb Raider')
+print(query)
